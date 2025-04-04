78011e3d24bdca4eb6a0ae675a8a5a111006fb39
Emerson <emersonnjunior2006@gmail.com>
2025-04-04 20:54:35 -0300

    aula de c# 02

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+namespace SISTEMADOALUNO
+{
+//cd com a extencao completa, dotnet new com o nome, cd com o nome 
+class program{
+    static void Main(String[]args){
+        Console.WriteLine("qual o nome do aluno: ");
+        String nome = Console.ReadLine();
+
+        Console.WriteLine("digite a primeira nota: ");
+        double nota1 = double.Parse(
+            Console.ReadLine(),
+            CultureInfo.InvariantCulture
+        );
+        Console.WriteLine("digite a primeira nota: ");
+        double nota2 = double.Parse(
+            Console.ReadLine(),
+            CultureInfo.InvariantCulture
+        );
+    }
+}
+}
+
