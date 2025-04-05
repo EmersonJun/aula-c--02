11f39cb518f1e71a5e8758a87ca83fb3e29a9fb4
Emerson <emersonnjunior2006@gmail.com>
2025-04-04 21:43:02 -0300

    ex1

--- Aluno.cs
+++ Aluno.cs
@@ -3,23 +3,23 @@ using System.Text.Encodings.Web;
 namespace SISTEMADOALUNO{
     public class ALuno{
         public String Nome { get; set;} = "";//guarda o nome
-        public double nota1 { get; set;}
-        public double nota2 { get; set;}
+        public double Nota1 { get; set;}
+        public double Nota2 { get; set;}
         //construtor da classe aluno (recebe nota e nota)
         public ALuno (String nome, double nota1, double nota2){
-            nome = nome;
-            nota1 = nota1;
-            nota2 = nota2;
+            Nome = nome;
+            Nota1 = nota1;
+            Nota2 = nota2;
         }
 
         //metodoo de calcular
         public double calcularMedia(){
-            return (nota1 + nota2) / 2;
+            return (Nota1 + Nota2) / 2;
         }
         public void EXibirResultados(){
             double media = calcularMedia();
-            Console.WriteLine($"\naluno: {Nome}");
-            Console.WriteLine($"\nmedia: {media}");
+            Console.WriteLine($"\n\naluno: {Nome}");
+            Console.WriteLine($"media: {media}");
 
             //verificar se a media e maior que 6
             if (media >= 6){
